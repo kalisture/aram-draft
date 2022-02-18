50720e88030300472c74a0300dc127bc33be8047
= <=>
2022-02-19 10:16:05 +1100

    added command

--- bot.py
+++ bot.py
@@ -1,6 +1,25 @@
 import discord
+from discord.ext import commands
+import random
 
-client = discord.Client()
+
+
+client = discord.Client(command_prefix = "!")
+
+allChamps = {"Aatrox","Ahri","Akali","Akshan","Alistar","Amumu","Anivia","Annie","Aphelios","Ashe","Aurelion Sol","Azir",
+"Bard","Blitzcrank","Brand","Braum","Caitlyn","Camille","Cassiopeia","Cho'Gath","Corki","Darius","Diana","Draven","Dr. Mundo",
+"Ekko","Elise","Evelynn","Ezreal","Fiddlesticks","Fiora","Fizz","Galio","Gangplank","Garen","Gnar","Gragas","Graves","Gwen",
+"Hecarim","Heimerdinger","Illaoi","Irelia","Ivern","Janna","Jarvan Iv","Jax","Jayce","Jhin","Jinx",
+"Kai'Sa","Kalista","Karma","Karthus","Kassadin","Katarina","Kayle","Kayn","Kennen","Kha'Zix","Kindred","Kled","Kog'Maw",
+"Leblanc","Lee Sin","Leona","Lillia","Lissandra","Lucian","Lulu","Lux","Malphite","Malzahar","Maokai","Master Yi","Miss Fortune","Mordekaiser","Morgana",
+"Nami","Nasus","Nautilus","Neeko","Nidalee","Nocturne","Nunu & Willump","Olaf","Orianna","Ornn","Pantheon","Poppy","Pyke","Qiyana","Quinn",
+"Rakan","Rammus","Rek'Sai","Rell","Renata Glasc","Renekton","Rengar","Riven","Rumble","Ryze","Samira","Sejuani","Senna",
+"Seraphine","Sett","Shaco","Shen","Shyvana","Singed","Sion","Sivir","Skarner","Sona","Soraka","Swain","Sylas","Syndra",
+"Tahm Kench","Taliyah","Talon","Taric","Teemo","Thresh","Tristana","Trundle","Tryndamere","Twisted Fate","Twitch","Udyr",
+"Urgot","Varus","Vayne","Veigar","Vel'Koz","Vex","Vi","Viego","Viktor","Vladimir","Volibear","Warwick","Wukong","Xayah",
+"Xerath","Xin Zhao","Yasuo","Yone","Yorick","Yuumi","Zac","Zed","Zeri","Ziggs","Zilean","Zoe","Zyra",}
+
+
 
 @client.event
 async def on_ready():
@@ -14,7 +33,9 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-    if message.content.startswith('$who is the best?'):
-        await message.channel.send('Jess is!')
+
+@client.command()
+async def hello(message, my_arg):
+        await message.channel.send(f"Hello {my_arg}")
 
 client.run('OTQ0MTc5MTY1NzczNTkwNTU4.Yg91Xw.4TU4xEUQeCejtBBVIn1XhsnjC24')
